e019d718e9e7fd5ad7310e6b65bd39b10e869c5b
zoroskr <99093432+zoroskr@users.noreply.github.com>
2022-09-11 22:02:39 -0300

    Update MergeAcumulado.pas

--- MergeAcumulado.pas
+++ MergeAcumulado.pas
@@ -1,43 +1,43 @@
- Procedure minimo(Var todos: arr; Var datosMin: info);
+ Procedure minimo(Var todos: arr; Var datosMin: info); // se pasa el vector por referencia al igual que el dato
       Var
           indiceMin, i: Integer;
       Begin
           datosMin.cod := 999;
           indiceMin := -1;
-          For i := 1 To dimf Do
-              If (todos [i] <> Nil) Then
+          For i := 1 To dimf Do //Recorro todas las posiciones del vector
+              If (todos [i] <> Nil) Then // Si se termino la lista del vector o la lista esta vacia sigo
                   If (todos [i]^.dato.cod <= datosMin.Cod) Then//Esta sentencia define el criterio para acumular
                     Begin
                       datosMin := todos [i]^.dato;//Esta reasigna el minimo
                       indiceMin := i;
                     End;
-          If (indiceMin <> -1) Then
+          If (indiceMin <> -1) Then // si el vector no esta vacio o si no se termino de recorrer
             Begin
-              datosMin := todos [indiceMin]^.dato;
-              todos[indiceMin] := todos [indiceMin]^.sig;
+              datosMin := todos [indiceMin]^.dato; //me guardo el minimo
+              todos[indiceMin] := todos [indiceMin]^.sig; //avanzo a las siguiente posicion de la lista de donde saque el minimo
             End;
       End;
 
-  Procedure mergeAcumulador(ar: arr; Var nuevo: lstm);
+  Procedure mergeAcumulador(ar: arr; Var nuevo: lstm); // se pasa el vector por valor y la lista merge por referencia
       Var
           min: info;
           actual: Integer;
           totalcant: Integer;
           prodNuevo: infom;
       Begin
-          nuevo := Nil;
-          minimo(ar, min);
-          While (min.cod <> 999) Do
+          nuevo := Nil; //se inicializa la lista merge en nil
+          minimo(ar, min); //se busca un minimo
+          While (min.cod <> 999) Do //si el vector no esta vacio o no se termino
             Begin
-              actual := min.cod;
-              totalcant := 0;
-              While ((min.cod <> 999) And (actual = min.cod)) Do
+              actual := min.cod; //me guardo el codigo del dato actual
+              totalcant := 0; // inicializo la variable contadora en 0
+              While ((min.cod <> 999) And (actual = min.cod)) Do // mientras este leyendo la misma venta
                 Begin
-                  totalcant := totalcant + min.cant;
-                  minimo(ar, min);
+                  totalcant := totalcant + min.cant; //sumo la cantidad de la venta
+                  minimo(ar, min); //busco otro minimo
                 End;
-              prodNuevo.cant := totalcant;
+              prodNuevo.cant := totalcant; // asigno los campos
               prodNuevo.cod  := actual;
-              agregarAdelante(nuevo, prodNuevo);
+              agregarAdelante(nuevo, prodNuevo); //agrego atras o adelante en la lista
             End;
       End;
