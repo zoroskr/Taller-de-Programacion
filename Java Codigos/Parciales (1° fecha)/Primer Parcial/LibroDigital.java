734d74ed8cc6251ff8aa5fef20cde8bbf1e762ec
zoroskr <99093432+zoroskr@users.noreply.github.com>
2022-09-16 15:21:59 -0300

    Update LibroDigital.java

--- Java Codigos/Parciales (1° fecha)/Primer Parcial/LibroDigital.java
+++ Java Codigos/Parciales (1° fecha)/Primer Parcial/LibroDigital.java
@@ -1,16 +1,19 @@
 package Ejercicio1;
 
-public class LibroDigital extends Libro {
+public class LibroDigital extends Libro { // crea un Libro Digital a partir de un libro
 
-    private String formato;
+    // atributos
+    private String formato; 
     private double tamanio;
 
+    //constructor
     public LibroDigital(String formato, double tamanio, String titulo, double precio) {
         super(titulo, precio);
         this.formato = formato;
         this.tamanio = tamanio;
     }
 
+    // getters and setters clasicos
     public String getFormato() {
         return formato;
     }
@@ -27,12 +30,13 @@ public class LibroDigital extends Libro {
         this.tamanio = tamanio;
     }
 
+    // para obtener el precio total por peso
     private double precioPeso() {
-        return this.getTamanio() * 2.5;
+        return this.getTamanio() * 2.5; // retorna el tamaño por 2.5
     }
 
-    @Override
+    @Override // sobre escribe el metodo del Libro
     public double precioFinal() {
-        return this.getPrecio() + this.IVA() + this.precioPeso();
+        return this.getPrecio() + this.IVA() + this.precioPeso(); // retorna la suma del precio más el iva más el precio por peso
     }
 }
