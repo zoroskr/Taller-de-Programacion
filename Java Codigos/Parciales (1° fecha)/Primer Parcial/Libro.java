86519e552628fd046583f9df7e47d61648cdcdd4
zoroskr <99093432+zoroskr@users.noreply.github.com>
2022-09-16 15:18:43 -0300

    Update Libro.java

--- Java Codigos/Parciales (1° fecha)/Primer Parcial/Libro.java
+++ Java Codigos/Parciales (1° fecha)/Primer Parcial/Libro.java
@@ -1,19 +1,21 @@
 package Ejercicio1;
 
-public abstract class Libro {
-
-    private String titulo;
+public abstract class Libro { // se declara una clase abstracta
+    // se declaran los atributos
+    private String titulo; 
     private double precio;
     private String[] autores;
     private int dimlaut;
-
+    
+    // se declara el constructor
     public Libro(String titulo, double precio) {
         this.titulo = titulo;
         this.precio = precio;
-        this.autores = new String[7];
-        this.dimlaut = 0;
+        this.autores = new String[7]; // se crea un vector de 8 posiciones
+        this.dimlaut = 0; // se inicializa la dimL en 0 para recorrer el vector
     }
 
+    // setters and getters clasicos
     public int getDimlaut() {
         return dimlaut;
     }
@@ -46,13 +48,15 @@ public abstract class Libro {
         this.autores = autores;
     }
 
+    // crea un meotodo para calcular el IVA
     public double IVA() {
-        return this.getPrecio() * 0.21;
+        return this.getPrecio() * 0.21; // retorna la multiplicacion del precio ingresado por o.21
     }
 
+    // crea un metodo para agregar un autor
     public void agregarAutor(String autor) {
-        this.getAutores()[this.getDimlaut()] = autor;
-        this.setDimlaut(this.getDimlaut() + 1);
+        this.getAutores()[this.getDimlaut()] = autor; //guarda un autor en la posicion del vector "dimL"
+        this.setDimlaut(this.getDimlaut() + 1); // aumenta la dimL en 1
     }
 
     public abstract double precioFinal();
