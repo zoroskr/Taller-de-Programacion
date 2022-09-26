6d91b336324c74d2a7b10836fa381a4f70bb49d6
zoroskr <99093432+zoroskr@users.noreply.github.com>
2022-09-25 22:41:05 -0300

    Add files via upload

---
+++ balanzas.java
@@ -0,0 +1,45 @@
+/*
+Definir una clase para representar balanzas comerciales (para ser utilizadas en
+verdulerías, carnicerías, etc). Una balanza comercial sólo mantiene el monto y la cantidad
+de items correspondientes a la compra actual (es decir, no almacena los ítems de la
+compra). La balanza debe responder a los siguientes mensajes:
+
+- iniciarCompra(): inicia el monto y la cantidad de ítems para la compra actual.
+- registrarProducto(pesoEnKg, precioPorKg): recibe el “peso en kilos” del ítem comprado
+y su “precio por kilo”, y actualiza el estado de la balanza.
+- getMonto(): retorna el monto a pagar por la compra actual.
+- getResumenDeCompra(): retorna un String del siguiente estilo “Total a pagar X por la
+compra de Y productos” , donde X es el monto e Y es la cantidad de ítems de la compra.
+B- Realizar un programa que instancie una balanza e inicie una compra. Lea desde teclado
+información de los ítems comprados (peso en kg y precio por kg) hasta ingresar peso 0.
+Registre cada producto en la balanza. Al finalizar, informe el resumen de la compra.
+ */
+package tema3;
+
+/**
+ *
+ * @author elmun
+ */
+public class balanzas {
+    double monto;
+    int items;
+    double peso;
+    double precio;
+
+    public void iniciarCompra(double monto, int items){
+        this.monto = 0;
+        this.items = items;
+    }
+
+    public void registrarProducto (double peso, double precio){
+        this.monto = (peso * precio * items);
+    }
+    
+    public double getMonto (double monto){
+        return monto;
+    }
+    
+    public String getResumenDeCompra(){
+        return("Total a pagar" + monto + "por la compra de" + items + "productos");
+    }
+}
